bc7157b03efc7bdd5c5fdef4e4401ad41455ef80
azbagas <azharbagaskara1@gmail.com>
2025-02-25 10:38:06 +0700

    refactor: integration test

--- test/setup.go
+++ test/setup.go
@@ -2,17 +2,22 @@ package test
 
 import (
 	"database/sql"
+	"encoding/json"
+	"io"
 	"net/http"
 	"time"
 
 	"github.com/azbagas/url-shortening-backend/app"
 	"github.com/azbagas/url-shortening-backend/controller"
 	"github.com/azbagas/url-shortening-backend/helper"
+
 	// "github.com/azbagas/url-shortening-backend/middleware"
 	"github.com/azbagas/url-shortening-backend/repository"
 	"github.com/azbagas/url-shortening-backend/service"
 	"github.com/go-playground/validator/v10"
 )
+
+type ResponseBody map[string]interface{}
 
 func SetupTestDB() *sql.DB {
 	db, err := sql.Open("pgx", "postgres://postgres:password@localhost:5432/url_shortening_backend?sslmode=disable")
@@ -29,7 +34,7 @@ func SetupTestDB() *sql.DB {
 
 func SetupRouter(db *sql.DB) http.Handler {
 	validate := validator.New()
-	
+
 	userRepository := repository.NewUserRepository()
 	userService := service.NewUserService(userRepository, db, validate)
 	userController := controller.NewUserController(userService)
@@ -42,4 +47,13 @@ func SetupRouter(db *sql.DB) http.Handler {
 func TruncateTable(db *sql.DB, table string) {
 	_, err := db.Exec("TRUNCATE TABLE " + table + " RESTART IDENTITY")
 	helper.PanicIfError(err)
+}
+
+func ReadResponseBody(response *http.Response, responseBody *ResponseBody) {
+	body, _ := io.ReadAll(response.Body)
+	json.Unmarshal(body, responseBody)
+}
+
+func SetContentTypeJson(request *http.Request) {
+	request.Header.Set("Content-Type", "application/json")
 }
--- test/user_controller_test.go
+++ test/user_controller_test.go
@@ -1,8 +1,6 @@
 package test
 
 import (
-	"encoding/json"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,94 +9,101 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRegisterUserSuccess(t *testing.T) {
-	db := SetupTestDB()
-	TruncateTable(db, "users")
-	router := SetupRouter(db)
-
-	requestBody := strings.NewReader(`{
-		"name": "Koseki Bijou",
-		"email": "biboo@gmail.com",
-		"password": "password",
-		"passwordConfirmation": "password"
-	}`)
-	request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
-	request.Header.Add("Content-Type", "application/json")
-
-	recorder := httptest.NewRecorder()
-
-	router.ServeHTTP(recorder, request)
-
-	response := recorder.Result()
-	assert.Equal(t, 201, response.StatusCode)
-
-	body, _ := io.ReadAll(response.Body)
-	var responseBody map[string]interface{}
-	json.Unmarshal(body, &responseBody)
-
-	data := responseBody["data"].(map[string]interface{})
-	assert.NotNil(t, data["id"])
-	assert.Equal(t, "Koseki Bijou", data["name"])
-	assert.Equal(t, "biboo@gmail.com", data["email"])
-	assert.NotNil(t, data["photo"])
-}
-
-func TestRegisterUserValidationFailed(t *testing.T) {
-	db := SetupTestDB()
-	TruncateTable(db, "users")
-	router := SetupRouter(db)
-
-	requestBody := strings.NewReader(`{
-		"name": "Koseki Bijou",
-		"email": "invalid email",
-		"password": "password",
-		"passwordConfirmation": "password"
-	}`)
-	request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
-	request.Header.Add("Content-Type", "application/json")
-
-	recorder := httptest.NewRecorder()
-
-	router.ServeHTTP(recorder, request)
-
-	response := recorder.Result()
-	assert.Equal(t, 400, response.StatusCode)
-
-	body, _ := io.ReadAll(response.Body)
-	var responseBody map[string]interface{}
-	json.Unmarshal(body, &responseBody)
-
-	assert.Equal(t, "Validation error", responseBody["message"])
-
-	errors := responseBody["errors"].([]interface{})
-	assert.NotEqual(t, 0, len(errors))
-}
-
-func TestRegisterUserEmailAlreadyRegistered(t *testing.T) {
-	TestRegisterUserSuccess(t)
-
+func TestRegisterUser(t *testing.T) {
 	db := SetupTestDB()
 	router := SetupRouter(db)
 
-	requestBody := strings.NewReader(`{
-		"name": "Koseki Bijou",
-		"email": "biboo@gmail.com",
-		"password": "password",
-		"passwordConfirmation": "password"
-	}`)
-	request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
-	request.Header.Add("Content-Type", "application/json")
+	t.Run("Register success", func(t *testing.T) {
+		TruncateTable(db, "users")
 
-	recorder := httptest.NewRecorder()
+		requestBody := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "biboo@gmail.com",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
+		SetContentTypeJson(request)
 
-	router.ServeHTTP(recorder, request)
+		recorder := httptest.NewRecorder()
 
-	response := recorder.Result()
-	assert.Equal(t, 409, response.StatusCode)
+		router.ServeHTTP(recorder, request)
 
-	body, _ := io.ReadAll(response.Body)
-	var responseBody map[string]interface{}
-	json.Unmarshal(body, &responseBody)
+		response := recorder.Result()
+		assert.Equal(t, 201, response.StatusCode)
 
-	assert.Equal(t, "Email already registered", responseBody["message"])
+		var responseBody ResponseBody
+		ReadResponseBody(response, &responseBody)
+
+		data := responseBody["data"].(map[string]interface{})
+		assert.NotNil(t, data["id"])
+		assert.Equal(t, "Koseki Bijou", data["name"])
+		assert.Equal(t, "biboo@gmail.com", data["email"])
+		assert.NotNil(t, data["photo"])
+	})
+
+	t.Run("Validation failed", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		requestBody := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "invalid email",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
+		SetContentTypeJson(request)
+
+		recorder := httptest.NewRecorder()
+
+		router.ServeHTTP(recorder, request)
+
+		response := recorder.Result()
+		assert.Equal(t, 400, response.StatusCode)
+
+		var responseBody ResponseBody
+		ReadResponseBody(response, &responseBody)
+
+		assert.Equal(t, "Validation error", responseBody["message"])
+
+		errors := responseBody["errors"].([]interface{})
+		assert.NotEqual(t, 0, len(errors))
+	})
+
+	t.Run("Email already registered", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		// First request
+		requestBody1 := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "biboo@gmail.com",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request1 := httptest.NewRequest(http.MethodPost, "/api/users", requestBody1)
+		SetContentTypeJson(request1)
+		recorder1 := httptest.NewRecorder()
+		router.ServeHTTP(recorder1, request1)
+
+		// Second request
+		requestBody2 := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "biboo@gmail.com",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request2 := httptest.NewRequest(http.MethodPost, "/api/users", requestBody2)
+		SetContentTypeJson(request2)
+		recorder2 := httptest.NewRecorder()
+		router.ServeHTTP(recorder2, request2)
+
+		// Capture the second response
+		response := recorder2.Result()
+		assert.Equal(t, 409, response.StatusCode)
+
+		var responseBody ResponseBody
+		ReadResponseBody(response, &responseBody)
+
+		assert.Equal(t, "Email already registered", responseBody["message"])
+	})
 }
