3058a01b3aa64b15e49c6eff4f065afbacbd130f
azbagas <azharbagaskara1@gmail.com>
2025-03-05 17:20:48 +0700

    test: add tests for getting new access token with success and failure scenarios

--- test/user_controller_test.go
+++ test/user_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -280,6 +281,91 @@ func TestGetCurrentUser(t *testing.T) {
 		request := httptest.NewRequest(http.MethodGet, "/api/users/current", nil)
 		SetContentTypeJson(request)
 		request.Header.Set("Authorization", "Bearer " + "InvalidAccessToken")
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		response := recorder.Result()
+		assert.Equal(t, 401, response.StatusCode)
+
+		var responseBody ResponseBody
+		ReadResponseBody(response, &responseBody)
+		assert.NotNil(t, responseBody["message"])
+	})
+}
+
+func TestGetNewAccessToken(t *testing.T) {
+	db := SetupTestDB()
+	router := SetupRouter(db)
+	defer db.Close()
+
+	t.Run("Get new access token success", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		// First request: Register user
+		requestBody := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "biboo@gmail.com",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
+		SetContentTypeJson(request)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		// Second request: Login user
+		requestBody = strings.NewReader(`{
+			"email": "biboo@gmail.com",
+			"password": "password"
+		}`)
+		request = httptest.NewRequest(http.MethodPost, "/api/users/login", requestBody)
+		SetContentTypeJson(request)
+		// Set user agent
+		request.Header.Set("User-Agent", "Mozilla/5.0")
+		recorder = httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		// Get first access token
+		var responseBody ResponseBody
+		ReadResponseBody(recorder.Result(), &responseBody)
+		firstAccessToken := responseBody["data"].(map[string]interface{})["accessToken"].(string)
+		
+		// Extract refresh token from cookies
+		refreshToken := ""
+		for _, cookie := range recorder.Result().Cookies() {
+			if cookie.Name == "refreshToken" {
+				refreshToken = cookie.Value
+				break
+			}
+		}
+		assert.NotEmpty(t, refreshToken)
+
+		// Sleep for a moment to make sure the new access token is different
+		time.Sleep(500 * time.Millisecond)
+
+		// Third request: Get new access token
+		request = httptest.NewRequest(http.MethodPost, "/api/users/refresh", nil)
+		SetContentTypeJson(request)
+		request.AddCookie(&http.Cookie{Name: "refreshToken", Value: refreshToken})
+		recorder = httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		response := recorder.Result()
+		assert.Equal(t, 200, response.StatusCode)
+
+		ReadResponseBody(response, &responseBody)
+		data := responseBody["data"].(map[string]interface{})
+		assert.NotNil(t, data["accessToken"])
+		// Check if the new access token is different from the old one
+		newAccessToken := data["accessToken"].(string)
+		assert.NotEqual(t, firstAccessToken, newAccessToken)
+	})
+
+	t.Run("Failed get new access token", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		request := httptest.NewRequest(http.MethodPost, "/api/users/refresh", nil)
+		SetContentTypeJson(request)
+		request.AddCookie(&http.Cookie{Name: "refreshToken", Value: "InvalidRefreshToken"})
 		recorder := httptest.NewRecorder()
 		router.ServeHTTP(recorder, request)
 
