050ca7d488b7954190926e1bb3e796c53f1d8240
azbagas <azharbagaskara1@gmail.com>
2025-03-05 16:22:32 +0700

    test: add tests for getting current user with success and failure scenarios

--- test/user_controller_test.go
+++ test/user_controller_test.go
@@ -219,3 +219,75 @@ func TestLoginUser(t *testing.T) {
 		assert.NotNil(t, responseBody["message"])
 	})
 }
+
+func TestGetCurrentUser(t *testing.T) {
+	db := SetupTestDB()
+	router := SetupRouter(db)
+	defer db.Close()
+
+	t.Run("Get current user success", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		// First request: Register user
+		requestBody := strings.NewReader(`{
+			"name": "Koseki Bijou",
+			"email": "biboo@gmail.com",
+			"password": "password",
+			"passwordConfirmation": "password"
+		}`)
+		request := httptest.NewRequest(http.MethodPost, "/api/users", requestBody)
+		SetContentTypeJson(request)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		// Second request: Login user
+		requestBody = strings.NewReader(`{
+			"email": "biboo@gmail.com",
+			"password": "password"
+		}`)
+		request = httptest.NewRequest(http.MethodPost, "/api/users/login", requestBody)
+		SetContentTypeJson(request)
+		// Set user agent
+		request.Header.Set("User-Agent", "Mozilla/5.0")
+		recorder = httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		// Get the access token
+		var responseBody ResponseBody
+		ReadResponseBody(recorder.Result(), &responseBody)
+		accessToken := responseBody["data"].(map[string]interface{})["accessToken"].(string)
+
+		// Third request: Get current user
+		request = httptest.NewRequest(http.MethodGet, "/api/users/current", nil)
+		SetContentTypeJson(request)
+		request.Header.Set("Authorization", "Bearer " + accessToken)
+		recorder = httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		response := recorder.Result()
+		assert.Equal(t, 200, response.StatusCode)
+
+		ReadResponseBody(response, &responseBody)
+		data := responseBody["data"].(map[string]interface{})
+		assert.NotNil(t, data["id"])
+		assert.Equal(t, "Koseki Bijou", data["name"])
+		assert.Equal(t, "biboo@gmail.com", data["email"])
+		assert.NotNil(t, data["photo"])
+	})
+
+	t.Run("Get current user failed", func(t *testing.T) {
+		TruncateTable(db, "users")
+
+		request := httptest.NewRequest(http.MethodGet, "/api/users/current", nil)
+		SetContentTypeJson(request)
+		request.Header.Set("Authorization", "Bearer " + "InvalidAccessToken")
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+
+		response := recorder.Result()
+		assert.Equal(t, 401, response.StatusCode)
+
+		var responseBody ResponseBody
+		ReadResponseBody(response, &responseBody)
+		assert.NotNil(t, responseBody["message"])
+	})
+}
